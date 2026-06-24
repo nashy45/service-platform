e23d73498741113520ba835030727c737a0dbe3f
nashy45 <fombutuhn@gmail.com>
2026-06-24 12:37:44 +0100

    add

--- build.sh
+++ build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "==> Upgrading pip..."
 pip install --upgrade pip
+
+echo "==> Installing requirements..."
 pip install -r requirements.txt
 
+echo "==> Collecting static files..."
 python manage.py collectstatic --no-input
+
+echo "==> Running migrations..."
 python manage.py migrate
+
+echo "==> Build complete!"
